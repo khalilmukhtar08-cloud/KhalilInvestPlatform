063180055b3d8ba90539a6a3627d42dd9e1e06ac
khalilmukhtar08-cloud <khalilmukhtar08@gmail.com>
2025-11-08 18:21:08 +0100

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.mongodb+srv://khalilmukhtar08_db_user:WjLjbNWcT697qpbf@cluster0.j3uza6z.mongodb.net/?appName=Cluster0) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
 }
 
